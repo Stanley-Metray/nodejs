7915147241d5a4d114778174000f6c56c57e8c85
Stanley Metray <stanleymetray@gmail.com>
2024-02-11 21:17:11 +0530

    Running nodejs on port 4000

--- index.js
+++ index.js
@@ -1,29 +1,21 @@
+// ++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+// SHARPENER, RUNNING ON PORT 4000
+
 const http = require('http');
-const fs = require('fs');
-console.clear();
+
+
 const server = http.createServer((req,res)=>{
     const url = req.url;
     if(url==='/')
-        res.end('<h1>Welcome to custom API</h1>');
-    else if(url==='/api')
-    {
-        fs.readFile(`./myapi/user.json`, 'utf-8', (err,data)=>{
-            if(err)
-                console.log(err);
-            else
-            {
-                let str = "";
-                let result = Array.from(JSON.parse(data).students);
-                result.forEach(element => {
-                    str += `<h1>${element.name}</h1>`;
-                });
-
-                res.end(`<h1>${str}</h1>`);
-            }
-        });
-
-    }
+        res.end("<h1>hello, my name is Stanley</h1>");
 });
 
 
-server.listen(5000);
+server.listen(4000, ()=>{
+    console.log("Server is running on port 4000");
+});
+
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++
