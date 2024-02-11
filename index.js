c180ee0960f7cc77360f17a6e6619fa53a4af382
Stanley Metray <stanleymetray@gmail.com>
2024-02-11 20:37:12 +0530

    Node file stream completed

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
-const Student = {
-    id : 193256,
-    name : "Stanley",
-    degree : "BCA",
-    department : "Computer Science",
-    graduated : true
-}
+const http = require('http');
+const fs = require('fs');
+console.clear();
+const server = http.createServer((req,res)=>{
+    const url = req.url;
+    if(url==='/')
+        res.end('<h1>Welcome to custom API</h1>');
+    else if(url==='/api')
+    {
+        fs.readFile(`./myapi/user.json`, 'utf-8', (err,data)=>{
+            if(err)
+                console.log(err);
+            else
+            {
+                let str = "";
+                let result = Array.from(JSON.parse(data).students);
+                result.forEach(element => {
+                    str += `<h1>${element.name}</h1>`;
+                });
 
-console.log(Student);
+                res.end(`<h1>${str}</h1>`);
+            }
+        });
+
+    }
+});
+
+
+server.listen(5000);
