976ea185d62daa762f12a3cfa775933bfc7f96d7
Stanley Metray <stanleymetray@gmail.com>
2024-02-12 20:32:13 +0530

    express app created

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log("Hello App.js is running through, npm-start");
+const express = require('express');
+const app = express();
+
+app.use((req,res, next)=>{
+    console.log("In first middleware");
+    next();
+})
+
+app.use((req, res)=>{
+    console.log("In second middleware");
+    res.send('<h1>Welcome to express app</h1>');
+});
+
+
+app.listen(3000);
